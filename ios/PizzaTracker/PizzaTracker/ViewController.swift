5ba604b4f7bb61464bbd35e59c3669aa682daeae
Akram Hussein <akramhussein@gmail.com>
2016-09-30 00:47:01 +0100

    Added basic project for PizzaTracker on iOS

---
+++ ios/PizzaTracker/PizzaTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PizzaTracker
+//
+//  Created by Akram Hussein on 30/09/2016.
+//  Copyright © 2016 deepstreamHub GmbH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
