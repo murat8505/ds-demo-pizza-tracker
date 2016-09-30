192472925da93f1ac5c2b9a80d6b11efe7d6380f
Akram Hussein <akramhussein@gmail.com>
2016-09-30 01:03:01 +0100

    Moved TrackingViewController out of Storyboard

---
+++ ios/PizzaTracker/PizzaTracker/TrackingViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TrackingViewController.swift
+//  PizzaTracker
+//
+//  Created by Akram Hussein on 30/09/2016.
+//  Copyright © 2016 deepstreamHub GmbH. All rights reserved.
+//
+
+import UIKit
+
+class TrackingViewController: UIViewController {
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
