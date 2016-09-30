7bf390f382e42fc03846e19ad379d5854726578d
Akram Hussein <akramhussein@gmail.com>
2016-09-30 01:03:47 +0100

    Added Login View Controller

---
+++ ios/PizzaTracker/PizzaTracker/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  PizzaTracker
+//
+//  Created by Akram Hussein on 30/09/2016.
+//  Copyright © 2016 deepstreamHub GmbH. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
